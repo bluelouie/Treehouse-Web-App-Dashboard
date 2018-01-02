4f8326afc6a8fed5ea1f67518f2788dd131e1c19
Jose L Medina <jmedinaluis92@yahoo.com>
2018-01-02 03:57:53 -0600

    Ui progress made

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
 
 const closeButton = document.querySelector('.alert button');
+// LineChart-----------------------------------------------------------------------------------------------------------------------------------------------
 
 //---x-axis
 var days = [ '16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31' ]
@@ -12,74 +13,112 @@ var traffic = [ 500, 1000, 1500, 2000, 2500 ]
 var trafficData = [ 768, 1000, 800, 1500, 2000, 1680, 1800, 1300, 1700, 2500, 2300, 2200]
 
 
+var ctx = document.getElementById("myLineChart");
 
-var ctx = document.getElementById("myChart");
+var data = {
+  labels: days,
+  datasets: [{
+    label: "'Weekly'",
+    backgroundColor: 'rgba(221, 219, 234, .7)',
+    borderColor: 'rgba(221, 219, 234, .7)',
+    borderWidth: 2,
+    hoverBackgroundColor: 'rgba(221, 219, 234, .7)',
+    hoverBorderColor: 'rgba(221, 219, 234, .7)',
+    data: trafficData,
+    lineTension : 0,
+    pointBackgroundColor: 'white',
+  }]
+};
 
+var options = {
+  maintainAspectRatio: false
+};
 
+var myLineChart = new Chart(ctx, {
+  type: 'line',
+  options: options,
+  data: data
+});
 
-function chart() {
-  var myChart = new Chart(ctx, {
-    type: 'line',
-    responsive: true,
-    maintainAspectRatio: 'false',
+// LineChart-----------------------------------------------------------------------------------------------------------------------------------------------
 
-    data: {
+// Bar chart-----------------------------------------------------------------------------------------------------------------------------------------------
 
-      labels: days,
-      datasets: [
-        {
-          label: 'Weekly',
-          label: 'monthly',
+//---x-axis
+var mobile = [ 'S', 'M', 'T', 'W', 'Th', 'F', 'Sat' ]
 
-          data: trafficData,
-          lineTension: 0,
-          backgroundColor: 'rgba(221, 219, 234, .7)',
-        }
-      ]
-    }
-  });
-}
-chart();
-
-
-
-//
-// function chartLoad() {
-//   var chartDiv = document.querySelector('#myChart');
-//
-//   if (window.innerWidth > 600) {
-//     chartDiv.setAttribute('width', 1000);
-//     chartDiv.setAttribute('height', 250);
-//
-//   } else {
-//     chartDiv.setAttribute('width', 500);
-//     chartDiv.setAttribute('height', 250);
-//
-//   }
-// }
-//
-// function chartResize() {
-//   var chartDiv = document.querySelector('#myChart');
-//
-//   if (window.innerWidth > 600) {
-//     chartDiv.setAttribute('width', 1000);
-//     chartDiv.setAttribute('height', 250);
-//     chart();
-//
-//
-//   } else {
-//     chartDiv.setAttribute('width', 500);
-//     chartDiv.setAttribute('height', 250);
-//     chart();
-//   }
-// }
-//
-// chartLoad();
-// chart();
-//
-// window.onresize = chartResize;
+//---data
+var mobileData = [ 150, 250, 500, 300, 400, 200, 180 ]
 
 
+
+var ctxBar = document.getElementById("myBarChart");
+
+var dataBar = {
+  labels: mobile,
+  datasets: [{
+    backgroundColor: 'rgba(221, 219, 234, .7)',
+    borderColor: 'rgba(221, 219, 234, .7)',
+    borderWidth: 2,
+    hoverBackgroundColor: 'rgba(221, 219, 234, .7)',
+    hoverBorderColor: 'rgba(221, 219, 234, .7)',
+    data: mobileData,
+    pointBackgroundColor: 'white',
+  }]
+};
+
+var optionsBar = {
+  maintainAspectRatio: false,
+  legend: {
+    display: false
+  }
+};
+
+var myBarChart = new Chart(ctxBar, {
+  type: 'bar',
+  options: optionsBar,
+  data: dataBar
+});
+
+// Bar chart-----------------------------------------------------------------------------------------------------------------------------------------------
+
+// Circle chart-----------------------------------------------------------------------------------------------------------------------------------------------
+
+//---x-axis
+var mobile = [ 'Phones', 'Tablets', 'Desktop', ]
+
+//---data
+var mobileData = [ 100, 250, 500]
+
+
+
+var ctxCircle = document.getElementById("myCircleChart");
+
+var dataCircle = {
+  labels: mobile,
+  datasets: [{
+    backgroundColor: 'rgba(221, 219, 234, .7)',
+    borderColor: 'rgba(221, 219, 234, .7)',
+    borderWidth: 2,
+    hoverBackgroundColor: 'rgba(221, 219, 234, .7)',
+    hoverBorderColor: 'rgba(221, 219, 234, .7)',
+    data: mobileData,
+    pointBackgroundColor: 'white',
+  }]
+};
+
+var optionsCircle = {
+  maintainAspectRatio: false
+};
+
+// And for a doughnut chart
+var myDoughnutChart = new Chart(ctxCircle, {
+    type: 'doughnut',
+    data: dataCircle,
+    options: optionsCircle
+});
+
+// Circle chart-----------------------------------------------------------------------------------------------------------------------------------------------
 window.addEventListener('load', setTimeout(function (e) {
   const a = document.querySelector('.alert');
   a.style.display = 'flex';
